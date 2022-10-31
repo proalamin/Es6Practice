5933651944091312811d095edf4010a3d5cccee8
proalamin <pro.alamin247@gmail.com>
2022-10-31 19:23:43 +0600

    for Of Lopp practice

---
+++ basicObject.js
@@ -0,0 +1,16 @@
+var billGates = {shirt: true, shirtColor:"OffWhite", smile: true, sunglass: true, sunglassColor: "White Glass"};
+
+var billGatesPro={
+    shirt:{
+        color:"offwhite",
+        quality: "Premium",
+        price:"200$"
+    },
+     
+    swatter:{
+        color: "gray",
+        quality: "Premium",
+        price: "2130$"
+    }
+
+}
