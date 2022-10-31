e7ddba2114c7ca516e5b497d54964b8df18037c6
proalamin <pro.alamin247@gmail.com>
2022-11-01 01:04:08 +0600

    function

---
+++ decisionMaking.js
@@ -0,0 +1,11 @@
+var billGates = { shirt: true, shirtColor: "OffWhite", smile: true, sunglass: true, sunglassColor: "White Glass" };
+
+if(billGates['shirtColor']== "red"){
+    console.log("shirt color is red")
+}
+else if(billGates['shirtColor']== "blue"){
+    console.log("shirt color is red")
+}
+else{
+    console.log('shit color not found')
+}
---
+++ function.js
@@ -0,0 +1,23 @@
+function myName(x,y){
+    var z= x+y;
+    console.log(z);
+}
+// myName(1, 2);
+
+// Rest Parameters Functions
+function restParameter(...x){
+//  console.log(x[6]);
+}
+
+// restParameter(1, 3, 5 ,7 ,8,4,3, 'A', 'B', 'C', 'D');
+
+// Function Returnsf
+function functionReturn(){
+
+    return "Bd";
+}
+console.log(functionReturn());
+
+
+
+
