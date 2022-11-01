5f839261d9b40170a79e1d85ea96e2d3a290ddac
proalamin <pro.alamin247@gmail.com>
2022-11-02 02:39:32 +0600

    Es6 Static Keyword

---
+++ Es6-Static-Keyword.js
@@ -0,0 +1,17 @@
+// class myClass{
+//      helllo(){
+//         console.log("Hello world")
+//     }
+// }
+
+// var obj= new myClass();
+// obj.helllo();
+
+
+class myClass{
+    static helllo(){
+        console.log("Hello world")
+    }
+}
+
+myClass.helllo();
