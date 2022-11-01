3f646d612409d7575d363d8e32fe9f35573641e2
proalamin <pro.alamin247@gmail.com>
2022-11-01 14:08:48 +0600

    simple-array & Multidimensional Arrays

---
+++ multidimensional-Arrays.js
@@ -0,0 +1,7 @@
+var bangladesh=['Dhaka', 'Ctg', 'Rangpur'];
+var india = ["mumbai", 'chennai', 'kolkata'];
+var china = ['Uhan','Shanghai'];
+
+var asia = [bangladesh, india, china];
+// console.log(asia);
+console.log(asia[1][2]);
---
+++ simple-array.js
@@ -0,0 +1,8 @@
+var myArray= ['as', 'b', 'c', 'r', 't', 'u', 'o','p'];
+
+var myArrayPro = new Array('as', 'b', 'c', 'r', 't', 'u', 'o', 'p');
+ 
+// console.log(myArray[1]);
+for(let item of myArrayPro){
+    console.log(item)
+}
